8072d480a8a9c4c40d4e2241743b13574f6c0be8
Asher Cohen <asherlc@asherlc.com>
2016-09-12 10:28:40 -0700

    fix deprecation

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ module.exports = {
   name: 'ember-cli-raw-handlebars',
   init: function() {
     checker.assertAbove(this, '2.4.3');
+
+    this._super.init && this._super.init.apply(this, arguments);
   },
   included: function(app) {
   },
